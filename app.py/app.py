c4e632319039ab3d3674f872423971a04fcfc866
sanjeevini mekin <sanjeevinimekin@gmail.com>
2026-03-06 20:03:24 +0530

    initial commit

---
+++ app.py/app.py
@@ -0,0 +1,15 @@
+from gpiozero import LED
+from time import sleep
+
+VERSION = "1.0"
+
+led = LED(17)
+
+print(f"Running Version {VERSION}")
+
+while True:
+    led.on()
+    sleep(1)
+    led.off()
+    sleep(1)
+    
